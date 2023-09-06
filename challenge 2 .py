664e61aa3bc1eea79a39335cf069727251cff3c8
autbru2622cs051 <144232558+autbru2622cs051@users.noreply.github.com>
2023-09-06 17:01:23 +0530

    Add files via upload

---
+++ challenge 2 .py
@@ -0,0 +1,7 @@
+#Online Aychon compiler (interpreter) to run Python online.
+#write Python 3 code in this online editor and run it.
+year = 2023 
+if(year == 0):
+     print("Leap year")
+else:
+     print("Not Leap year")
